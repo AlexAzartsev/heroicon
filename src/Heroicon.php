d1dd54fc3954e282926e4a15d347c42891446d7c
Benjamin <christianbfc97@gmail.com>
2021-08-24 12:48:34 -0500

    option to disable edition and only outline icons

--- src/Heroicon.php
+++ src/Heroicon.php
@@ -12,4 +12,22 @@ class Heroicon extends Field
      * @var string
      */
     public $component = 'heroicon';
+    public $editable = true;
+
+    public function disableEdition()
+    {
+        $this->editable = false;
+        return $this->withMeta(['editable' => $this->editable]);
+    }
+
+    public function onlyOutline()
+    {
+        return $this->withMeta(['only_outline' => true]);
+    }
+
+    public function jsonSerialize()
+    {
+        $this->withMeta(['editable' => $this->editable]);
+        return parent::jsonSerialize();
+    }
 }
